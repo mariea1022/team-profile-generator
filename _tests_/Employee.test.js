d13ba46c38e7b7098155701c0a6012cacd51d9be
Marie Lewis <mariea1022@gmail.com>
2022-12-06 17:23:47 -0800

    added jest as a script in package.json; wrote test for Employee which passed

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,2 +1,50 @@
 const Employee = require("../lib/Employee")
-  
+
+// creates employee object 
+describe('Employee', () => {
+    describe('Initialization', () => {
+        it('should create an employee object', () => {
+            // arange
+            const employee = new Employee('Jared', 1000, 'jared@gmail.com')
+
+            // act & assert?
+            expect(employee.name).toEqual(expect.any(String));
+            expect(employee.id).toEqual(expect.any(Number));
+            expect(employee.email).toEqual(expect.any(String));
+});
+
+// // gets name from this.name() 
+//         test('should get employee name', () => {
+//             // arange
+//             const employee = new Employee('Jared, 1000, jared@gmail.com')
+
+//             // act & assert?
+//             expect(employee.getName()).toEqual(expect.any(String));
+// });
+
+// gets id from getId() 
+            it('should get employee ID', () => {
+                // arrange
+                const employee = new Employee('Jared', 1000, 'jared@gmail.com')
+                
+                // act & assert?
+                expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+            // gets emails from getEmail()
+            it('should get employee email', () => {
+                // arrange
+                const employee = new Employee('Jared', 1000, 'jared@gmail.com')
+
+                expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+            // gets role from getRole()
+            it('should get role of employee', () => {
+            // arrange
+                const employee = new Employee('Jared', 1000, 'jared@gmail.com')
+
+                expect(employee.getRole()).toEqual("Employee");
+})
+})
+})
--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,1 +1,31 @@
 const Engineer = require("../lib/Engineer")
+
+describe('Employee', () => {
+    describe('Initialization', () => {
+        it('should create an Engineer object', () => {
+            // arange
+            const Engineer = new Engineer ('Holly', 1234, 'holly@abcd.com', 'hollyDev');
+
+            // act & assert?
+            expect(engineer.name).toEqual(expect.any(String));
+            expect(engineer.id).toEqual(expect.any(String));
+            expect(engineer.email).toEqual(expect.any(String));
+});
+
+});
+            // gets emails from getEmail()
+            it('should get Engineer GitHub', () => {
+                // arrange
+                const Engineer = new Engineer ('Holly', 1234, 'holly@abcd.com', 'hollyDev');
+
+                expect(engineer.github).toEqual(expect.any(String));
+});
+            // gets role from getRole()
+            it('should get role of employee', () => {
+            // arrange
+                const Engineer = new Engineer ('Holly', 1234, 'holly@abcd.com', 'hollyDev');
+
+                expect(engineer.getRole()).toEqual("Engineer");
+})
+})
+})
